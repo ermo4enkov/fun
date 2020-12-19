52f5ce8066fa2662cf1b26ded054e3520b3b4435
r_neumann <ermo4enkov@gmail.com>
2020-12-19 20:51:50 +0100

    8ball

--- 8ball.rb
+++ 8ball.rb
@@ -1,3 +1,9 @@
+greetings = [
+    "Hallo mein lieber freund, ich antworte auf ihren frage",
+    "Who asks, recieve an answer",
+    "Hallo, ich habe diese antwort für dich"
+]
+
 answers = [
     "cool",
     "bad",
@@ -5,5 +11,8 @@ answers = [
     "smth",
     "0111001"
 ]
+
+puts "#{greetings.sample}
+ "
 
 puts answers.sample
