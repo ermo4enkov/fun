23b5df1fa539736b6e06d4a76cb4adebf3c8704d
r_neumann <ermo4enkov@gmail.com>
2020-12-20 20:49:45 +0100

    Reverse array

---
+++ array_invert.rb
@@ -0,0 +1,8 @@
+origin_arr = [1, 2, 3, 4, 5]
+reverse_arr = origin_arr.reverse
+
+puts "origin_arr: #{origin_arr}"
+puts "reverse_arr: #{reverse_arr}"
+
+origin_arr.reverse!
+puts "reversed origin: #{origin_arr}"
---
+++ array_merge.rb
@@ -0,0 +1,6 @@
+first = ["Gena", "George", "Misha"]
+second = ["Katrin", "Liza", "Masha"]
+
+third = first.concat(second)
+
+puts third
