e4df40d3cbf820e6e3c5549e627efd104b11a731
r_neumann <ermo4enkov@gmail.com>
2020-12-26 15:03:54 +0100

    count_lines

--- lesson13/count_lines.rb
+++ lesson13/count_lines.rb
@@ -5,6 +5,16 @@ file = File.new(file_path, 'r');
 
 lines = file.readlines
 
+empty_string = 0;
+
 lines.each { |line|
-  puts line.length
+  if line.length < 2
+    empty_string += 1
+  end
 }
+
+
+
+puts "All the lines #{lines.length}"
+puts "Empty strings #{empty_string}"
+puts "Last five lines #{lines.last(5)}"
