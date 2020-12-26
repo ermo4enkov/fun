575ae71a1c07eb6aba68310cb5ea9de10ce471ee
r_neumann <ermo4enkov@gmail.com>
2020-12-26 14:39:23 +0100

    File exist?

--- lesson13/file.rb
+++ lesson13/file.rb
@@ -1,7 +1,16 @@
 file = File.new('../lesson13/example.txt', "r")
 
-content = file.read
+# content = file.read
+# The whole content
 lines = file.readlines
 
-puts content
-puts lines
+# puts content
+puts lines.sample
+# By the line
+#
+#
+if File.exist?('../lesson13/example.txt')
+  # here I can work with file
+else
+  puts 'I did not find file'
+end
