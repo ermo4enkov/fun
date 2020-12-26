c93f1dfb7f79b1f675b6228a16e914754f5276b8
r_neumann <ermo4enkov@gmail.com>
2020-12-26 14:43:52 +0100

    File path

--- lesson13/file.rb
+++ lesson13/file.rb
@@ -1,16 +1,18 @@
-file = File.new('../lesson13/example.txt', "r")
+current_path = File.dirname(__FILE__)
+file_path = current_path + "/example.txt"
+file = File.new(file_path  , "r")
 
-# content = file.read
+content = file.read
 # The whole content
-lines = file.readlines
+# lines = file.readlines
 
-# puts content
-puts lines.sample
+puts content
+# puts lines.sample
 # By the line
 #
 #
-if File.exist?('../lesson13/example.txt')
-  # here I can work with file
-else
-  puts 'I did not find file'
-end
+# if File.exist?('../lesson13/_example.txt')
+#   # here I can work with file
+# else
+#   puts 'I did not find file'
+# end
