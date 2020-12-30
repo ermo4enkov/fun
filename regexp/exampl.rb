21a306ebfe8446d5e7b482995b6e8f4a46caf888
r_neumann <ermo4enkov@gmail.com>
2020-12-30 09:34:12 +0100

    quantifiers

--- regexp/exampl.rb
+++ regexp/exampl.rb
@@ -1,5 +1,32 @@
 p 'cat' =~ /c.t/
 # => 0
 
+# The dot means here should be ONE and only one uniq character
+puts "Dot"
 p 'caat' =~ /c.t/
 # => nil
+#
+
+#The anchor ^ means that strings should start with specific character
+p 'cat' =~ /^c.t/
+# => 0
+p 'cat' =~ /^a.t/
+# => nil
+
+#The $ means that strings should end with specific character
+p 'cat' =~ /^c.t$/
+# => 0
+p 'cat' =~ /^c.d$/
+# # => nil
+#
+# Quantifier "*" - any characters quantity from 0 to infinity
+puts 'Quanifier *'
+p 'cat' =~ /^c.t*$/
+
+# Quantifier "+" - characters quantity from 1 to infinity
+puts "Quanifier '+' - checks symbols from 1 to infinity"
+p 'cabsdsdst' =~ /^c.+t$/
+
+p 'cassssssst' =~ /c.+t$/
+
+
