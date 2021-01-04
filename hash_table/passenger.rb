cbf5b1013b0a8fa6a8b3ae4a2377b155a774db54
r_neumann <ermo4enkov@gmail.com>
2021-01-04 16:46:35 +0100

    Passenger

---
+++ hash_table/passenger.rb
@@ -0,0 +1,9 @@
+passenger = {
+    ticket_number: '123',
+    first_name: 'Venedikt',
+    family_name: 'Erofeev',
+    path: 'Stockholm - Rome',
+    passport: '43 2323 23232'
+}
+
+passenger.each { |item| puts item }
