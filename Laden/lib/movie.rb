00032ace709f0d188d68ac0f2a203e33515c0362
r_neumann <ermo4enkov@gmail.com>
2021-01-06 22:43:28 +0100

    Film

--- Laden/lib/movie.rb
+++ Laden/lib/movie.rb
@@ -1,3 +1,13 @@
 class Movie < Product
+  attr_reader :title, :year, :director
 
+  def initialize(params)
+    @title = params[:title]
+    @year = params[:year]
+    @director = params[:director]
+  end
+
+  def to_s
+    puts "Film #{title}, #{year}, #{director}"
+  end
 end
--- Laden/main.rb
+++ Laden/main.rb
@@ -2,6 +2,8 @@ require_relative 'lib/product'
 require_relative 'lib/movie'
 require_relative 'lib/book'
 
-film = Movie.new(price: 290, quantity: 5)
+film = Movie.new(price: 290, quantity: 5, title: 'Title', year: 2020, director: 'Me')
 
-puts film.price
+film.to_s
+
+
