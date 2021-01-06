f8468f33025edb2053e668c65f47572ab6d5aed3
r_neumann <ermo4enkov@gmail.com>
2021-01-06 22:23:45 +0100

    Static method in Class

---
+++ Classes_static_abstract/main.rb
@@ -0,0 +1,8 @@
+class MyClass
+  def self.static_method
+    puts "I am static method"
+  end
+end
+
+# I did not create new object from Class. Just called the MyClass directly
+MyClass.static_method
